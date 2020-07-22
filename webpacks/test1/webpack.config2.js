cf961e09ac660b81e7bf06d317548ed5b8b71b13
chenxuan <chenxuanvictory@163.com>
2020-07-23 00:06:29 +0800

    123

--- webpacks/test1/webpack.config2.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-module.exports = {
-    entry: ['./public/index1.js', './public/index2.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'), //形成一个绝对经
-        filename: '[name].js'
-    }
-}
